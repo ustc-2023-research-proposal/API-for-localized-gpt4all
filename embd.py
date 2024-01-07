74e62a24357d744bf642a0a48563da0b854151c3
MinamiNamida <141938569+MinamiNamida@users.noreply.github.com>
2024-01-07 15:39:39 +0800

    以及更新的embed测试程序

--- embd.py
+++ embd.py
@@ -1,19 +1,22 @@
 import requests
 import json
 
-def getEmbedding(text:str) :
-
-    url = 'http://127.0.0.1:6008/embed?' +'text=' + text
-    r = requests.get(url=url, verify=False)
-    return json.loads(r.text)
-
-def PostEmbedding(text:str) :
+def PostEmbedding(texts) :
     url = 'http://127.0.0.1:6008/embed'
-    data = {'text' : text}
-    r = requests.post(url=url, data=data)
-    return json.loads(r.text)
+
+    data = {
+        'input' : texts,
+        'model' : 'hello',
+    }
+
+    # 需要加入,在header中声明是以json形式来传输数据的
+    headers = {
+        'Content-Type': 'application/json',
+    }
+    data = json.dumps(data)
+    r = requests.post(url=url, data=data, headers=headers)
+    return r.text
 
 if __name__ == '__main__':
-    print(getEmbedding('Hey? How are you?'))
-
-    print(PostEmbedding('maybe?'))
+    
+    print(PostEmbedding(['hello','how are you']))
